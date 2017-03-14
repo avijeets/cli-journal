b200a6ba616668f03daf01269989105a0bcb6452
avijeet <a@avijeets.com>
2017-03-13 22:29:15 -0400

    condensed + added db

--- diary.py
+++ diary.py
@@ -1,13 +1,22 @@
+import datetime
 from peewee import *
 
 db = SqliteDatabase('diary.db')
 
-class Entry(Model):
-    #content
-    #timestamp
 
-    clas Meta:
+class Entry(Model):
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
         database = db
+
+
+def initialize():
+    """Create the db and table if they don't exist"""
+    db.connect()
+    db.create_tables([Entry], safe=True)
+
 
 def menu_loop():
     """Show menu"""
@@ -16,11 +25,13 @@ def menu_loop():
 def add_entry():
     """Add an entry"""
 
+
 def view_entries():
     """View previous entries"""
+
 
 def delete_entry(entry):
     """Delete an entry"""
 
 if __name__ == '__main__':
-    menu_loop()
+    initialize()
